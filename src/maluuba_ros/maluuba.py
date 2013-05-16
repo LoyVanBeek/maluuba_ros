90ee0cea76f7e843958f7c83582512b074ef9720
Loy van Beek <loy.vanbeek@gmail.com>
2013-05-16 20:39:14 +0200

    Added basics for Normalize api, but somehow my installation does not match with the example on the site. Basic code complains about status: return NAPIClient.NormalizeResponse(**(self.__query('normalize', phrase=phrase, type=_type, timezone=timezone))); TypeError: __init__() got an unexpected keyword argument 'status'

--- src/maluuba_ros/maluuba.py
+++ src/maluuba_ros/maluuba.py
@@ -7,30 +7,44 @@ import maluuba_ros.srv
 import sys
 
 import rospy
-import std_msgs
 
 from maluuba_napi import client
 
 class Maluuba(object):
-    """docstring for Interpreter"""
+    """Wrapper for the Maluuba natural language understanding API.
+    the nAPI supports two services, interpret and normalize. """
     def __init__(self, key):
         c = client.NAPIClient(key)
         super(Maluuba, self).__init__()
         self.client = c
     
         rospy.Service('maluuba/interpret', maluuba_ros.srv.Interpret, self.interpret)
+        #rospy.Service('maluuba/normalize', maluuba_ros.srv.Normalize, self.normalize)
 
+        self.processors = {"REMINDER_SET":self._parse_REMINDER_SET}
 
     def interpret(self, request):
-        response = self.client.interpret(request.sentence)
+        response = self.client.interpret(request.phrase)
 
         return maluuba_ros.srv.InterpretResponse(
             str(response.entities), 
             str(response.category), 
             str(response.action))
 
+    def default_parser(self, entities):
+        return str(entities)
+
+    def _parse_REMINDER_SET(self, entities):
+        pass
+
     def normalize(self, request):
-        response = self.client.normalize(request.data)
+        response = self.client.normalize(request.phrase, request.type, request.timezone)
+
+        return maluuba_ros.srv.NormalizeResponse(
+            str(response.entities), 
+            str(response.category), 
+            str(response.action))
+
 
 if __name__ == "__main__":
     rospy.init_node("maluuba_node")
