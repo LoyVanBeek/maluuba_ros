6fd33deeb752bf4afb8d325fe98b03ae919ff3b5
Loy van Beek <loy.vanbeek@gmail.com>
2013-06-05 19:38:17 +0200

    Refactored the service response to a separate Interpretation-message. listen and interpret now publishes this message to a topic, for anyone to listen an respons to interpretations it can process. Moved the listen_and_interpret-node to the src dir

---
+++ src/maluuba_ros/listen_and_interpret.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+'''Run ~/ros/fuerte/tue/user/sjoerd/google_speech_to_text/src recognizer.py, this works best somehow. Very experimental'''
+
+import roslib
+roslib.load_manifest('maluuba_ros')
+import rospy
+
+from std_msgs.msg import String
+from maluuba_ros.srv import Interpret
+from maluuba_ros.msg import Interpretation
+
+rospy.init_node("listen_and_interpret")
+
+service = rospy.ServiceProxy('maluuba/interpret', Interpret)
+publisher = rospy.Publisher("maluuba/interpretations", Interpretation)
+
+
+def interpret_string(msg):
+    try:
+        response = service(msg.data)
+        interpretation = response.interpretation
+        display = str(interpretation)
+        lines = display.split("""\n""")
+        display = "\n".join(
+            line for line in lines if "''" not in line and '[]' not in line)
+        
+        rospy.logdebug(display)
+        publisher.publish(interpretation)
+
+    except Exception, e:
+        rospy.logerr(e)
+
+subcriber = rospy.Subscriber("/speech/output", String, interpret_string)
+
+rospy.spin()
